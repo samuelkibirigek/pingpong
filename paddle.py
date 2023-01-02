4c5fe47e30ec50caf32aee1750f07ae414ea8fc8
Kibirige Kalule Samuel <samuelkibirigek@gmail.com>
2023-01-02 23:30:48 +0300

    The paddle class and various methods controlling the paddle movements, appearance and functionality

--- paddle.py
+++ paddle.py
@@ -13,11 +13,13 @@ class Paddle(Turtle):
         self.goto(position)
 
     def go_up(self):
-        new_y = self.ycor() + 20
-        self.goto(self.xcor(), new_y)
+        if self.ycor() < 280:
+            new_y = self.ycor() + 20
+            self.goto(self.xcor(), new_y)
 
     def go_down(self):
-        new_y = self.ycor() - 20
-        self.goto(self.xcor(), new_y)
+        if self.ycor() > -280:
+            new_y = self.ycor() - 20
+            self.goto(self.xcor(), new_y)
 
 
