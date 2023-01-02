b15357abe67cbc3adb199e0831b805d45c40398b
Kibirige Kalule Samuel <samuelkibirigek@gmail.com>
2023-01-02 23:31:48 +0300

    The main file, still in progress but here everything comes together to give ping pong

--- main.py
+++ main.py
@@ -6,6 +6,9 @@
 """
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
+from score import Score
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -16,6 +19,8 @@ screen.tracer(0)
 
 l_paddle = Paddle()
 r_paddle = Paddle()
+ball = Ball()
+score = Score()
 
 l_paddle.position((-350, 0))
 r_paddle.position((350, 0))
@@ -27,7 +32,26 @@ screen.onkey(r_paddle.go_down, "Down")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(ball.the_speed)
     screen.update()
+    ball.move()
+    score.scoreboard()
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    if ball.xcor() > 370:
+        if score.l_score == 10:
+            score.game_over()
+        score.update_l_score()
+        ball.reset_position()
+        ball.bounce_x()
+    if ball.xcor() < -370:
+        score.game_over()
+        score.update_r_score()
+        ball.reset_position()
+        ball.bounce_x()
 
 
 screen.exitonclick()
